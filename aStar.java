d225ec2035d529e4a3eb65d0b69bfdc7127ac40b
Rob Moor <81611195+robamoor@users.noreply.github.com>
2022-11-18 19:58:34 +0000

    movefn, heursiticfn, costfn

--- aStar.java
+++ aStar.java
@@ -5,7 +5,7 @@ public class aStar{
   public aStar(){
     
   }
-
+  //move fn (number of correct final conditions)
   public int moveFunction(worldState ws){
     ArrayList<String> currentWorldState = ws.getWorldState();
     String monkeyRoom = currentWorldState.get(0);
@@ -20,10 +20,10 @@ public class aStar{
     
     int gn = 0;
     if(monkeyRoom != monkeyGoalRoom){
-      gn = gn+2;
+      gn++;
     }
     if(boxRoom != boxGoalRoom){
-      gn = gn+2;
+      gn++;
     }
     if(monkeyHeight != monkeyGoalHeight){
       gn++;
@@ -31,14 +31,45 @@ public class aStar{
     if(monkeyHasBananas == goalState.get(3)){
       gn = 0;
     }
-  System.out.println(gn);
+    System.out.println(gn);
     return gn;
   }
 
-    /*
-cost fn = move + heuristic
-move fn (number of correct final conditions)
-heuristic (moving/moving box are high cost and climb/grab bananas is low)
+  //heuristic (moving/moving box are high cost and climb/grab bananas is low)
+  public int heuristicFunction(worldState ws){
+    ArrayList<String> currentWorldState = ws.getWorldState();
+    String monkeyRoom = currentWorldState.get(0);
+    String boxRoom = currentWorldState.get(1);
+    String monkeyHeight = currentWorldState.get(2);
+    String monkeyHasBananas = currentWorldState.get(3);
+
+    ArrayList<String> goalState = ws.getGoalState();
+    String monkeyGoalRoom = goalState.get(0);
+    String boxGoalRoom = goalState.get(1);
+    String monkeyGoalHeight = goalState.get(2);
+    
+    int hn = 0;
+    if(monkeyRoom != monkeyGoalRoom){
+      hn = hn+3;
+    }
+    if(boxRoom != boxGoalRoom){
+      hn= hn+3;
+    }
+    if(monkeyHeight != monkeyGoalHeight){
+      hn++;
+    }
+    if(monkeyHasBananas == goalState.get(3)){
+      hn = 0;
+    }
+    System.out.println(hn);
+    return hn;
+  }
+
+  public int costFunction(int gn, int hn){
+    return gn+hn;
+  }
+
+/*
 
 start from initial conditions-add to open node list
 evaluate open node neighbors-move possible?/moveBox possible?/climb up/down possible?/grabBananas possible? what are their cost values
