6529ee1fe8b320d5c2042faa0170a4cdb7265d30
Rob Moor <81611195+robamoor@users.noreply.github.com>
2022-11-18 19:47:35 +0000

    delecting class files

--- aStar.java
+++ aStar.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-public class aStar {
+public class aStar{
 
-  public aStar() {
-
+  public aStar(){
+    
   }
 
-  public int moveFunction(worldState ws) {
+  public int moveFunction(worldState ws){
     ArrayList<String> currentWorldState = ws.getWorldState();
     String monkeyRoom = currentWorldState.get(0);
     String boxRoom = currentWorldState.get(1);
@@ -17,34 +17,33 @@ public class aStar {
     String monkeyGoalRoom = goalState.get(0);
     String boxGoalRoom = goalState.get(1);
     String monkeyGoalHeight = goalState.get(2);
-
+    
     int gn = 0;
-    if (monkeyRoom != monkeyGoalRoom) {
-      gn = gn + 2;
+    if(monkeyRoom != monkeyGoalRoom){
+      gn = fn+2;
     }
-    if (boxRoom != boxGoalRoom) {
-      gn = gn + 2;
+    if(boxRoom != boxGoalRoom){
+      gn = fn+2;
     }
-    if (monkeyHeight != monkeyGoalHeight) {
+    if(monkeyHeight != monkeyGoalHeight){
       gn++;
     }
-    if (monkeyHasBananas == goalState.get(3)) {
+    if(monkeyHasBananas == goalState.get(3)){
       gn = 0;
     }
-    System.out.println(gn);
+  System.out.println(gn);
     return gn;
   }
 
-  /*
-   * cost fn = move(g(n)) + heuristic(h(n))
-   * move fn (number of correct final conditions)
-   * heuristic (moving/moving box are high cost and climb/grab bananas is low)
-   * 
-   * start from initial conditions-add to open node list
-   * evaluate open node neighbors-move possible?/moveBox possible?/climb up/down
-   * possible?/grabBananas possible? what are their cost values
-   * 
-   * pick lowest cost
-   * repeat
-   */
+    /*
+cost fn = move + heuristic
+move fn (number of correct final conditions)
+heuristic (moving/moving box are high cost and climb/grab bananas is low)
+
+start from initial conditions-add to open node list
+evaluate open node neighbors-move possible?/moveBox possible?/climb up/down possible?/grabBananas possible? what are their cost values
+
+pick lowest cost 
+repeat
+  */
 }
