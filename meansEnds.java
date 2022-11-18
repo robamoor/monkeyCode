9adf0dbe481236d68bee7d20b37a37ab399a8767
robamoor <81611195+robamoor@users.noreply.github.com>
2022-11-18 11:34:11 -0800

    Create meansEnds.java

---
+++ meansEnds.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class meansEnds{
+  private Stack<Integer> goalStack;
+  private int finalGoal;
+  
+  public meansEnds(Stack<Integer> goalStack, int finalGoal){
+    this.goalStack = goalStack;
+    this.finalGoal = finalGoal;
+  }
+/*
+  public findGoals(){
+    
+  }
+
+  public getGoalStack(){
+    return goalStack;
+  }
+
+  public int getFinalGoal{
+    
+    return finalGoal;
+  }
+*/
+
+  
+}
