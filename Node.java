4778815b3c75fef22d17d388dce824b3ecd892e0
robamoor <81611195+robamoor@users.noreply.github.com>
2022-11-20 21:41:03 +0000

    indentation

--- Node.java
+++ Node.java
@@ -31,35 +31,34 @@ public class Node implements Comparable<Node>{
     }
 
       //move fn (number of correct final conditions)
-  public int moveFunction(){
-    ArrayList<String> currentWorldState = ws.getWorldState();
-    String monkeyRoom = currentWorldState.get(0);
-    String boxRoom = currentWorldState.get(1);
-    String monkeyHeight = currentWorldState.get(2);
-    String monkeyHasBananas = currentWorldState.get(3);
+    public int moveFunction(){
+      ArrayList<String> currentWorldState = ws.getWorldState();
+      String monkeyRoom = currentWorldState.get(0);
+      String boxRoom = currentWorldState.get(1);
+      String monkeyHeight = currentWorldState.get(2);
+      String monkeyHasBananas = currentWorldState.get(3);
 
-    //be careful to check what this is
-    ArrayList<String> goalState = ws.getGoalState();
-    String monkeyGoalRoom = goalState.get(0);
-    String boxGoalRoom = goalState.get(1);
-    String monkeyGoalHeight = goalState.get(2);
+      //be careful to check what this is
+      ArrayList<String> goalState = ws.getGoalState();
+      String monkeyGoalRoom = goalState.get(0);
+      String boxGoalRoom = goalState.get(1);
+      String monkeyGoalHeight = goalState.get(2);
     
-    int gn = 0;
-    if(monkeyRoom != monkeyGoalRoom){
-      gn++;
+      int gn = 0;
+      if(monkeyRoom != monkeyGoalRoom){
+        gn++;
+      }
+      if(boxRoom != boxGoalRoom){
+        gn++;
+      }
+      if(monkeyHeight != monkeyGoalHeight){
+        gn++;
+      }
+      if(monkeyHasBananas == goalState.get(3)){
+        gn = 0;
+      }
+      return gn;
     }
-    if(boxRoom != boxGoalRoom){
-      gn++;
-    }
-    if(monkeyHeight != monkeyGoalHeight){
-      gn++;
-    }
-    if(monkeyHasBananas == goalState.get(3)){
-      gn = 0;
-    }
-    System.out.println(gn);
-    return gn;
-  }
 
     //heuristic (moving/moving box are high cost and climb/grab bananas is low)
     public int heuristicFunction(){
@@ -87,7 +86,7 @@ public class Node implements Comparable<Node>{
         if(monkeyHasBananas == goalState.get(3)){
           hn = 0;
         }
-        System.out.println(hn);
+
         return hn;
       }
 
