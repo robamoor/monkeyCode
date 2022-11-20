fb44b99a4a7a7fcab56ed3a73bd9b90f506e356f
Jasonl2398 <51425860+Jasonl2398@users.noreply.github.com>
2022-11-20 13:20:16 -0800

    moved fns from aStar to Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 
-public class Node {
+public class Node implements Comparable<Node>{
     private worldState ws;
     private ArrayList<Node> children;
+    private int moveScore;
+    private int hScore;
+
     //num children = number of possible actions from world state
     public Node(worldState ws) {
         this.ws = ws;
+        this.moveScore = moveFunction();
+        this.hScore = heuristicFunction();
         children = new ArrayList<>();
     }
 
@@ -24,6 +29,90 @@ public class Node {
     public worldState getWorldState() {
         return this.ws;
     }
+
+      //move fn (number of correct final conditions)
+  public int moveFunction(){
+    ArrayList<String> currentWorldState = ws.getWorldState();
+    String monkeyRoom = currentWorldState.get(0);
+    String boxRoom = currentWorldState.get(1);
+    String monkeyHeight = currentWorldState.get(2);
+    String monkeyHasBananas = currentWorldState.get(3);
+
+    //be careful to check what this is
+    ArrayList<String> goalState = ws.getGoalState();
+    String monkeyGoalRoom = goalState.get(0);
+    String boxGoalRoom = goalState.get(1);
+    String monkeyGoalHeight = goalState.get(2);
+    
+    int gn = 0;
+    if(monkeyRoom != monkeyGoalRoom){
+      gn++;
+    }
+    if(boxRoom != boxGoalRoom){
+      gn++;
+    }
+    if(monkeyHeight != monkeyGoalHeight){
+      gn++;
+    }
+    if(monkeyHasBananas == goalState.get(3)){
+      gn = 0;
+    }
+    System.out.println(gn);
+    return gn;
+  }
+
+    //heuristic (moving/moving box are high cost and climb/grab bananas is low)
+    public int heuristicFunction(){
+        ArrayList<String> currentWorldState = ws.getWorldState();
+        String monkeyRoom = currentWorldState.get(0);
+        String boxRoom = currentWorldState.get(1);
+        String monkeyHeight = currentWorldState.get(2);
+        String monkeyHasBananas = currentWorldState.get(3);
+    
+        ArrayList<String> goalState = ws.getGoalState();
+        String monkeyGoalRoom = goalState.get(0);
+        String boxGoalRoom = goalState.get(1);
+        String monkeyGoalHeight = goalState.get(2);
+        
+        int hn = 0;
+        if(monkeyRoom != monkeyGoalRoom){
+          hn = hn+3;
+        }
+        if(boxRoom != boxGoalRoom){
+          hn= hn+3;
+        }
+        if(monkeyHeight != monkeyGoalHeight){
+          hn++;
+        }
+        if(monkeyHasBananas == goalState.get(3)){
+          hn = 0;
+        }
+        System.out.println(hn);
+        return hn;
+      }
+
+      public int getMoveScore() {
+        return moveScore;
+      }
+
+      public int getHScore() {
+        return hScore;
+      }
+
+
+    
+    public int compareTo(Node node) {
+        int cost1 = moveScore + hScore;
+        int cost2 = node.getMoveScore() + node.getHScore();
+        int max = cost1;
+        if (cost1 < cost2) {
+            max = cost2;
+        }
+        return max;
+    }
+    
+
+
 }
 
 
--- aStar.java
+++ aStar.java
@@ -5,66 +5,6 @@ public class aStar{
   public aStar(){
     
   }
-  //move fn (number of correct final conditions)
-  public int moveFunction(worldState ws){
-    ArrayList<String> currentWorldState = ws.getWorldState();
-    String monkeyRoom = currentWorldState.get(0);
-    String boxRoom = currentWorldState.get(1);
-    String monkeyHeight = currentWorldState.get(2);
-    String monkeyHasBananas = currentWorldState.get(3);
-
-    //be careful to check what this is
-    ArrayList<String> goalState = ws.getGoalState();
-    String monkeyGoalRoom = goalState.get(0);
-    String boxGoalRoom = goalState.get(1);
-    String monkeyGoalHeight = goalState.get(2);
-    
-    int gn = 0;
-    if(monkeyRoom != monkeyGoalRoom){
-      gn++;
-    }
-    if(boxRoom != boxGoalRoom){
-      gn++;
-    }
-    if(monkeyHeight != monkeyGoalHeight){
-      gn++;
-    }
-    if(monkeyHasBananas == goalState.get(3)){
-      gn = 0;
-    }
-    System.out.println(gn);
-    return gn;
-  }
-
-  //heuristic (moving/moving box are high cost and climb/grab bananas is low)
-  public int heuristicFunction(worldState ws){
-    ArrayList<String> currentWorldState = ws.getWorldState();
-    String monkeyRoom = currentWorldState.get(0);
-    String boxRoom = currentWorldState.get(1);
-    String monkeyHeight = currentWorldState.get(2);
-    String monkeyHasBananas = currentWorldState.get(3);
-
-    ArrayList<String> goalState = ws.getGoalState();
-    String monkeyGoalRoom = goalState.get(0);
-    String boxGoalRoom = goalState.get(1);
-    String monkeyGoalHeight = goalState.get(2);
-    
-    int hn = 0;
-    if(monkeyRoom != monkeyGoalRoom){
-      hn = hn+3;
-    }
-    if(boxRoom != boxGoalRoom){
-      hn= hn+3;
-    }
-    if(monkeyHeight != monkeyGoalHeight){
-      hn++;
-    }
-    if(monkeyHasBananas == goalState.get(3)){
-      hn = 0;
-    }
-    System.out.println(hn);
-    return hn;
-  }
 
   public int costFunction(int gn, int hn){
     return gn+hn;
@@ -78,7 +18,7 @@ public class aStar{
     openList.add(start);
     return start;
   }
-  
+
 /* 
 A* pseudocode
 
