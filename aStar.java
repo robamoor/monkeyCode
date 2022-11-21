f318d9ef4fcfb68e6a34439927fbeaa30d163cda
robamoor <81611195+robamoor@users.noreply.github.com>
2022-11-21 07:44:08 +0000

    holy holy

--- aStar.java
+++ aStar.java
@@ -79,47 +79,4 @@ public class aStar{
     return monkeyHasBananasCheck&&heightCheck&&bananasRoomCheck&&boxRoomCheck&&monkeyRoomCheck;
   }
 
-/* 
-A* pseudocode
-
-make an empty list C of closed nodes
-make a list O of open nodes and their respective fn values containing the start node
-while O isn't empty:
-    pick a node n from O with the best value for fn
-    if n is target:
-        return solution
-    for every m which is a neighbor of n:
-        if (m is not in C) and (m is not in O):
-            add m to O, set n as m's parent
-            calculate g(m) and f(m) and save them
-        else:
-            if f(m) from last iteration is better than g(m) from this iteration:
-                set n as m's parent
-                update g(m) and f(m)
-                if m is in C:
-                    move m to O
-    move n from O to C
-
-return that there's no solution */
-
-/*public static void printPath(Node target){
-    Node n = target;
-
-    if(n==null)
-        return;
-
-    List<Integer> ids = new ArrayList<>();
-
-    while(n.parent != null){
-        ids.add(n.id);
-        n = n.parent;
-    }
-    ids.add(n.id);
-    Collections.reverse(ids);
-
-    for(int id : ids){
-        System.out.print(id + " ");
-    }
-    System.out.println("");
-*/
 }
